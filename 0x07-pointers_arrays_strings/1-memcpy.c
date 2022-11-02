9a565aa0e5ebf7756c0e4704150652c3d59022b3
nabww <nwambulwa@yahoo.com>
2022-11-02 02:49:59 -0700

    0 correction

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,7 +11,11 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < n; i++)
-		dest[i] = src[i];
+	 i =  0;
+	 while (i < n)
+	 {
+		 *(dest + i) = *(src + i);
+		 i++;
+	 }
 	return (dest);
 }
