8a212fd4a2b1af433ce74132d431dff2116517e5
nabww <nwambulwa@yahoo.com>
2022-11-03 01:38:06 -0700

    number 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _pow_recursion - check code
+ * @x: int
+ * @y: int
+ * return: power of recursion
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)/*base cdtn*/
+	{
+		return (-1);
+	}
+	else if (y != 0)
+		return (x * _pow_recursion(x, y-1));
+	else
+		return (1);
+}
