69f6c9ce8b300bbfc97caaa95de53a0c1d8b1b2f
nabww <nwambulwa@yahoo.com>
2022-11-02 21:57:19 -0700

    number 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,31 +2,32 @@
 
 
 /**
- * main: check code
- * return: string
+ * _strstr - check code
+ * @str1: variable
+ * @str2: variable
+ * @needle:var
+ * @haystack: var
+ * Return: string
  *
  */
 
 char *_strstr(char *needle, char *haystack)
 {
-	int i;
-	int j;
-
-	while (*(haystack + i))
+	char *str1, *str2;
+	while (*haystack != '\0')
 	{
-		if (*(haystack + i) == *(needle + j))
+		str1 = haystack;
+		str2 = needle;
+
+		while (haystack != '\0' && *str2 != '\0' && *haystack == *str2)
 		{
-			j++;
-			if (*(needle + j) == '\0')
-			{
-				return ((haystack + i) - (j - 1));
-			}
-		}else 
-				j = 0;
-			i++;
+			haystack++;
+			str2++;
+		}
+		if *str2 == '\0')
+			return (str1);
+		haystack = str1 + 1;
 	}
-	if (needle == '\0')
-			return (haystack);
 	return (0);
 
 }
