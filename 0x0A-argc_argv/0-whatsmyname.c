6a716ce4fc75c9544e3f3671042233dca5b13aaa
nabww <nwambulwa@yahoo.com>
2022-11-07 02:24:27 -0800

    number 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/**
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * #argv: arguments
+ **/
+
+int main (int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
