183901b17ec31018208aff7b30ba9545fcf5d4c8
nabww <nwambulwa@yahoo.com>
2022-11-08 01:16:47 -0800

    number 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,67 @@
+#include "main.h"
+
+/**
+ * _strlen - array
+ * @s: arr
+ * Return - 1
+ **/
+
+int _strlen(char *s)
+{
+	unsigned int i;
+
+	i = 0;
+	while(s[i] != '\0')
+	{
+		i++;
+	}
+	return(i);
+}
+
+/**
+ * _strcpy - copy
+ * @src: array
+ * @dest: dest arr
+ * Return - dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
+ * _strdup - arr
+ *@str: arr
+ *Return - p
+ **/
+char *_strdup(char *str)
+{
+	char *dst;
+	unsigned int size;
+
+	if (str == 0)
+	{
+		return (NULL);
+	}
+
+	size = _strlen(str) +1;
+
+	dst = (char *) malloc(size *sizeof(char));
+
+	if (dst == 0)
+	{
+		return (NULL);
+	}
+	_strcpy(dst, str);
+	return(dst);
+}
