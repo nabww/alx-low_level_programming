76560f0f6bb36766e647b7345dff133d5fb16b02
nabww <nwambulwa@yahoo.com>
2022-11-11 22:34:17 -0800

    number 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * malloc_checked - prints string
+ * @b: var
+ *Return: void
+ **/
+
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return (p);
+}
