3f879d89ba5d06822ce40dd815a8f79b823acdf9
nwambulwa <nwambulwa@yahoo.com>
2022-11-28 01:36:33 -0800

    numbers 100 nd 101

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * bmain - check code
+ * return: no return
+ **/
+
+void __attribute__((constructor)) bmain()
+{
+	printf("Your're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
