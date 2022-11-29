65f2776323b28948ff0a7a5ed6c46bd5bc678687
nwambulwa <nwambulwa@yahoo.com>
2022-11-28 21:40:46 -0800

    correction no. 0

--- 0x12-singly_linked_lists/main.c
+++ 0x12-singly_linked_lists/main.c
@@ -1,4 +1,7 @@
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
+#include "lists.h"
 
 /**
  *  * main - check the code
@@ -7,6 +10,31 @@
  *     */
 int main(void)
 {
-	    printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-	        return (0);
+	    list_t *head;
+	        list_t *new;
+		    list_t hello = {"World", 5, NULL};
+		        size_t n;
+
+			    head = &hello;
+			        new = malloc(sizeof(list_t));
+				    if (new == NULL)
+					        {
+							        printf("Error\n");
+								        return (1);
+									    }
+				        new->str = strdup("Hello");
+					    new->len = 5;
+					        new->next = head;
+						    head = new;
+						        n = print_list(head);
+							    printf("-> %lu elements\n", n);
+
+							        printf("\n");
+								    free(new->str);
+								        new->str = NULL;
+									    n = print_list(head);
+									        printf("-> %lu elements\n", n);
+
+										    free(new);
+										        return (0);
 }
