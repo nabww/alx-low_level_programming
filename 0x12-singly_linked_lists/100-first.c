77a3a0417453a12e40a7e22566241fb96f2a441a
nwambulwa <nwambulwa@yahoo.com>
2022-11-28 21:56:52 -0800

    correct number 100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,6 +7,6 @@
 
 void __attribute__((constructor)) bmain()
 {
-	printf("Your're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("Your're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!");
 }
