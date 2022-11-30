f45e6eb2a8bcd014cc63426a1db1581ada68848b
nwambulwa <nwambulwa@yahoo.com>
2022-11-30 05:37:13 -0800

    number 9

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * sum_listint - check code
+ * @head: first node
+ * Return: sum
+ **/
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *temp = head;
+
+	while (temp)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+	return (sum);
+}
