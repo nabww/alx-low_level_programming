dd7efa054c7723d32d36f3f091d45ae1124b2b9d
nwambulwa <nwambulwa@yahoo.com>
2022-12-10 07:18:43 -0800

    no. 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,7 +16,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (totalSize == NULL)
 		return (0);
 	if (filename == NULL)
-		return(0);
+		return (0);
 
 	file = open(filename, O_RDONLY);
 	if (file == -1)
