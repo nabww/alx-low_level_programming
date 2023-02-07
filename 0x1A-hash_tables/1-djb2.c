20366636503e4acabe1c83e04feeeec300054def
nwambulwa <nwambulwa@yahoo.com>
2023-02-07 04:22:16 -0800

    number 1

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,18 @@
+/**
+ * hash_djb2 - Hash function
+ * @str: the string
+ * Return: the hash number
+ **/
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c;
+	}
+	return (hash);
+}
